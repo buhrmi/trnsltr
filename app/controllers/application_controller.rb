ae285b62cf31da9ac892b877ad4b2b02657e59f8
Stefan Buhrmester <buhrmi@gmail.com>
2020-07-10 03:59:50 +0100

    fix crash when not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,14 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :current_repo, :github
 
   def current_user
-    @current_user ||= github.users.get
+    @current_user ||= github.users.get if github
   end
 
   def current_repo
     session[:repo]
   end
 
-
-
   def github
-    @github ||= Github.new oauth_token: session[:token]
+    @github ||= Github.new oauth_token: session[:token] if session[:token]
   end
 end
