8180666098f9308f29bf535e362cccaa723e1081
Stefan Buhrmester <buhrmi@gmail.com>
2020-07-10 04:16:20 +0100

    add repos rout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sessions
   resources :translations
-  
+  resources :repos
+    
   get ':org/:repo', to: 'repos#show'
 
   root to: 'sessions#new'
